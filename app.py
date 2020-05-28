dd27797e32345d5b3a4829db6dada64bafbe74b7
qel0s <46157522+qel0s@users.noreply.github.com>
2020-05-28 21:34:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def show_notification(map):
     toaster.show_toast('CSGO Current Map Notifier','The map has changed to ' + map,'./steam.ico',duration=5)
 
 def get_map():
-    ip_addres = '216.52.148.47'
+    ip_addres = 'ip_addres'
     port = '27015'
     link = 'https://www.gametracker.com/server_info/' + ip_addres +':'+ port + '/'
     r = requests.get(link)
